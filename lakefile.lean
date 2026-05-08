1c4b301c3d270310286374010f485f87eab99710
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-05-07 22:00:54 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,13 @@
 import Lake
 open Lake DSL
 
-package "OPN" where
+package "opn"
 
 @[default_target]
-lean_lib "OPN" where
+lean_lib «OPN-Proof1»
+
+@[default_target]
+lean_lib «OPN-Proof2»
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.26.0"
